335f43881e4b84e122156c58586c598c667301df
matteo di lucchio <matteo.dilucchio@edu.unito.it>
2019-05-26 23:33:26 +0200

    visibilità e zero value

--- 002/002.go
+++ 002/002.go
@@ -12,11 +12,20 @@ func main() {
 
 	c := 20 // variabile locale
 
-	//il tipo viene assegnato in fase di compilazione, non serve specificarlo
+	// il tipo viene assegnato in fase di compilazione, non serve specificarlo
+	// una variabile non inizializzata (b) contiene il valore zero di default per il suo tipo
 
 	fmt.Println(a + c)
 
 	//variabile ridichiarata perchè è prioritaria da quella dichiarata fuori dal main
 	b := "hey"
+	fmt.Println(b)
+
+	foo()
+}
+
+func foo() {
+	// b è quella dichiarata fuori dal main (variabile globale)
+	// e contiene il suo valore "zero" perchè non è stata inizializzata
 	fmt.Println(b)
 }
