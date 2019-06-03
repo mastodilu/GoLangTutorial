89d0161310bdded0362ee40ef7d48db7e7dbd270
Di Lucchio Matteo <matteo.dilucchio@beta80group.it>
2019-06-03 16:43:02 +0200

    040

---
+++ 040/040.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+)
+
+func factorialNonRecursive(n int) int {
+	fact := 1
+	for i := 1; i <= n; i++ {
+		fact *= i
+	}
+	return fact
+}
+
+func factorial(n int) int {
+	if n == 1 {
+		return 1
+	}
+	return n * factorial(n-1)
+}
+
+func main() {
+	fmt.Println("factorial 10:", factorial(10))
+	fmt.Println("factorial 10:", factorialNonRecursive(10))
+}
