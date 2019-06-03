e4d1494536772c7f8d31c526835ef98859cf6736
Di Lucchio Matteo <matteo.dilucchio@beta80group.it>
2019-06-03 11:30:43 +0200

    030

--- 030/030.go
+++ 030/030.go
@@ -4,12 +4,17 @@ import (
 	"fmt"
 )
 
-func yess(msg ...string) {
-	fmt.Println(msg)
+// riceve uno slice di string
+func yell(msg ...string) {
+	fmt.Printf("number of parameters: %d\n", len(msg))
+	for i, value := range msg {
+		fmt.Printf("i:%d - %s\n", i, value)
+	}
 }
 
 func main() {
 	// variadic parameters
+	fmt.Println("VARIADIC PARAMTERS ARE SLICES!!\n")
 	yell("hello")
 	yell("Hi", "bye")
 	yell("1", "2", "3", "asdafdsfadfaljkdfljk")
