8504efe5d9712c27ab16ac0c68754ca846aa4062
Di Lucchio Matteo <matteo.dilucchio@beta80group.it>
2019-06-03 15:39:33 +0200

    038

--- 038/038.go
+++ 038/038.go
@@ -4,39 +4,59 @@ import (
 	"fmt"
 )
 
-func foo() string {
-	return "hi"
+func sum(numbers ...int) int {
+	sum := 0
+	for _, v := range numbers {
+		sum += v
+	}
+	return sum
+}
+
+func reviewOldStuff() {
+	fmt.Println(sum(1, 2, 3, 4, 5))
+
+	numbers := []int{1, 2, 3, 4, 5}
+
+	fmt.Println(sum(numbers...))
+}
+
+// numeri pari
+func even(f func(xi ...int) int, vi ...int) int {
+	yi := []int{}
+	for _, v := range vi {
+		if v%2 == 0 {
+			yi = append(yi, v)
+		}
+	}
+	return f(yi...)
 }
 
 func main() {
-	a := func() func() int {
-		return func() int {
-			return 42
+	reviewOldStuff()
+
+	evenNumbers := func(min, max int) func() []int {
+		return func() []int {
+			even := []int{}
+
+			for i := min; i <= max; i++ {
+				if i%2 == 0 {
+					even = append(even, i)
+				}
+			}
+			return even
 		}
+
 	}
-	fmt.Printf("type of a: %T\n", a)
 
-	b := a()
-	fmt.Printf("Type of b: %T\n", b)
+	total := sum(evenNumbers(1, 5)()...)
+	fmt.Println(total)
 
-	c := b()
-	fmt.Printf("Type of c: %T\n", c)
+	// --------------------------------------
 
-	// -----------------------------------
-
-	f1 := func() int {
-		return func() int {
-			return 5
-		}()
-	}
-	fmt.Println(f1())
-
-	// -----------------------------------
-
-	f2 := func() func() int {
-		return func() int {
-			return 5
-		}
-	}
-	fmt.Println(f2()())
+	// 💥 callback
+	// una callback è una funzione che viene passata come parametro di un'altra funzione
+	numbers := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	total = sum(numbers...)
+	s := even(sum, numbers...)
+	fmt.Println(s)
 }
