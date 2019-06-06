594b2925870ebcbf4dc8574d6e3827fa0710ac09
matteo di lucchio <matteo.dilucchio@edu.unito.it>
2019-06-06 23:45:32 +0200

    196

--- MoreExercises/196/textanalyze/main.go
+++ MoreExercises/196/textanalyze/main.go
@@ -9,17 +9,29 @@ import (
 // Wordsmap is a map that counts the number of occurrences for each word in the text
 var Wordsmap map[string]int
 
+// wordsCount stores the total numbers of words.
+var wordsCount int
+
 // CountWords counts each words using a map.
 func CountWords(text string) {
 	Wordsmap = make(map[string]int)
 	sslice := strings.Fields(text)
+
+	wordsCount = len(sslice)
+
 	for _, word := range sslice {
 		Wordsmap[word]++
 	}
 }
 
+// WordsNumber returns the total number of words in the quote.
+func WordsNumber() int {
+	return wordsCount
+}
+
 // PrintMap prints the whole map in a readable way
 func PrintMap() {
+	fmt.Println("There are", wordsCount, "words in total.")
 	for k, v := range Wordsmap {
 		fmt.Println(k, v)
 	}
